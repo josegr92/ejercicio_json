1a8f9c82cba29c3006772c7204bdbcda891a4f99
Jose Garcia <josegarciaroldan@gmail.com>
2015-02-26 20:39:11 +0100

    He realizado el punto 1 del programa areas-recreativas.py

--- areas-recreativas.py
+++ areas-recreativas.py
@@ -5,3 +5,7 @@ import json
 fich=open("areas-recreativas.json","r")
 doc=json.load(fich)
 fich.close()
+
+for directorio in doc["directorios"]["directorio"]:
+	print "Nombre: %s"%directorio["nombre"]["content"]
+	print ":Dirección: %s"%directorio["direccion"]
